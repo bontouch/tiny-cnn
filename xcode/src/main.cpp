d698ec9fedac846f0a50b3c31f9f4635b823e8d9
Roland Persson <roland.persson@bontouch.com>
2016-06-08 11:57:53 +0200

    Added Xcode project for working on tiny cnn.

---
+++ xcode/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tiny_cnn
+//
+//  Created by Roland Persson on 08/06/16.
+//  Copyright © 2016 Bontouch AB. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
